ec181b312168a03b7ce8a8ccb3078e4067e331ca
Ivan Urra <ivan.urra.wd@gmail.com>
2021-06-26 13:27:17 +0200

    state

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,20 +1,27 @@
 <template>
   <div class="row">
       <div class="col-12">
-          card
+          {{paises}}
       </div>
   </div>
 </template>
 
 <script>
-import { onMounted } from '@vue/runtime-core'
+import { computed, onMounted } from '@vue/runtime-core'
 import {useStore} from 'vuex'
 export default {
     setup(){
         const store = useStore()
+
+        const paises = computed (() => {
+            return store.state.paises
+        })
+
         onMounted(() => {
             store.dispatch('getPaises')
         })
+
+        return {paises}
     }
 }
 </script>
