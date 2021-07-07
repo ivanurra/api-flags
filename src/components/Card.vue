f2a08700ec1ac921993943ff7e42747e2f4d1171
ivanurra <ivan.urra.wd@gmail.com>
2021-07-07 17:04:18 +0200

    format numbers

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="card">
+  <div class="card mb-4">
       <div class="card-body">
           <h5>
               {{pais.name}}
           </h5>
           <p class="card-text">
               <span class="badge badge-dark d-block mb-1">Name: {{pais.nativeName}}</span><br>
-              <span class="badge badge-info p-3 d-block mb-1">Population: {{pais.population}}</span><br>
+              <span class="badge badge-info p-3 d-block mb-1">Population: {{numeroFormato(pais.population)}}</span><br>
               <span class="badge badge-dark d-block mb-1">Capital: {{pais.capital}}</span><br>
               <span class="badge badge-dark d-block mb-1">Region: {{pais.region}}</span>
           </p>
@@ -16,6 +16,13 @@
 
 <script>
 export default {
-    props: ['pais']
+    props: ['pais'],
+    setup(){
+        const numeroFormato = (num) => {
+            return new Intl.NumberFormat("de-DE").format(num)
+        }
+
+        return {numeroFormato}
+    }
 }
 </script>
