fb38f9f55d2ff03814f02cd5a5584dc3cdd6ba42
Ivan Urra <ivan.urra.wd@gmail.com>
2021-07-09 13:25:01 +0200

    filter by continent

--- src/components/Continents.vue
+++ src/components/Continents.vue
@@ -2,16 +2,27 @@
 <div class="text-center" style="margin-bottom: 40px;">
   <h3 class="text-center">Filter by Continent</h3>
   <div class="btn-group" role="group" aria-label="Basic example">
-    <button type="button" class="btn btn-dark">AM</button>
-    <button type="button" class="btn btn-dark">EU</button>
-    <button type="button" class="btn btn-dark">AS</button>
-    <button type="button" class="btn btn-dark">OC</button>
-    <button type="button" class="btn btn-dark">AF</button>
-    <button type="button" class="btn btn-dark">ALL</button>
+    <button type="button" class="btn btn-dark" @click="filtro('Americas')">AM</button>
+    <button type="button" class="btn btn-dark" @click="filtro('Europe')">EU</button>
+    <button type="button" class="btn btn-dark" @click="filtro('Asia')">AS</button>
+    <button type="button" class="btn btn-dark" @click="filtro('Oceania')">OC</button>
+    <button type="button" class="btn btn-dark" @click="filtro('Africa')">AF</button>
+    <button type="button" class="btn btn-dark" @click="filtro('')">ALL</button>
   </div>
 </div>
 </template>
 
 <script>
-export default {};
+import { useStore } from 'vuex';
+export default {
+  setup() {
+    const store = useStore()
+
+    const filtro = (region) => {
+      store.dispatch('filtrarRegion', region)
+    }
+
+    return {filtro}
+  }
+};
 </script>
