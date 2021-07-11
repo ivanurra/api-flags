57ef4021204de3bb49652c75dc98eab45d9c9845
Ivan Urra <ivan.urra.wd@gmail.com>
2021-07-11 13:27:09 +0200

    input

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,22 @@
   <div class="container">
     <h2 class="text-center">COUNTRIES API</h2>
     <Continents />
+    <Buscador />
     <CardList />
   </div>
 </template>
 
 <script>
+import Buscador from './components/Buscador.vue';
 import CardList from "./components/CardList.vue";
 import Continents from './components/Continents.vue';
+
 export default {
   name: "App",
   components: {
     CardList,
     Continents,
+    Buscador
   },
 };
 </script>
---
+++ src/components/Buscador.vue
@@ -0,0 +1,24 @@
+<template>
+  <input 
+  type="text"
+  placeholder="Find a country"
+  class="form-control my-3"
+  v-model="texto"
+  @keyup="procesarInput"
+  >
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+export default {
+    setup(){
+        const texto = ref('')
+
+        const procesarInput = () => {
+            console.log(texto.value)
+        }
+
+        return {texto, procesarInput}
+    }
+}
+</script>
